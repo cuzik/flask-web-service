4113a49ba4e2281690f523490dcb106b9432ae85
carloscuzik <cuzikjack@gmail.com>
2017-12-28 14:14:24 -0200

    add insert new product

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
+import back_end as bd
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,7 +15,28 @@ class Info(Resource):
   def get(self):
     return open("info.txt", "r").read().split('\n')
 
+class InsertProduct(Resource):
+  def get(self, prd_cod, prd_name, prd_description, prd_price, prd_stock):
+    values = {
+      'prd_cod': prd_cod,
+      'prd_name': prd_name,
+      'prd_description': prd_description,
+      'prd_price': prd_price,
+      'prd_stock': prd_stock
+    }
+    try:
+      bd.insert_product(values)
+    except Exception as e:
+      return {'error': str(e)}
+    return [{'success': 'this product was included'}, values]
+
+
+
 api.add_resource(Info, '/', '/info')
+api.add_resource(InsertProduct, '/add/<string:prd_cod>/<string:prd_name>/<string:prd_description>/<string:prd_price>/<string:prd_stock>')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
